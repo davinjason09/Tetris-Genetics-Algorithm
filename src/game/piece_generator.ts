511f5e577d7ff283e4d09c6d505a76e9ff17544f
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-10-15 19:45:23 +0700

    feat(piece-generator): added piece generator

---
+++ src/game/piece_generator.ts
@@ -0,0 +1,38 @@
+import { Piece } from './piece';
+
+export class RandomPieceGenerator {
+  private bag: number[];
+  private index: number;
+
+  constructor() {
+    this.shuffleBag();
+    this.bag = [0, 1, 2, 3, 4, 5, 6];
+    this.index = -1;
+  }
+
+  public nextPiece(): Piece {
+    this.index++;
+
+    if (this.index >= this.bag.length) {
+      this.index = 0;
+      this.shuffleBag();
+    }
+
+    return Piece.fromIndex(this.bag[this.index]);
+  }
+
+  private shuffleBag(): void {
+    let currentIndex: number = this.bag.length;
+    let temporaryValue: number;
+    let randomIndex: number;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      temporaryValue = this.bag[currentIndex];
+      this.bag[currentIndex] = this.bag[randomIndex];
+      this.bag[randomIndex] = temporaryValue;
+    }
+  }
+}
