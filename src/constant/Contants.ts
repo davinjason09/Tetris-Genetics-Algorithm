1ed804da60edf066324544eb455a54a13a60b7ac
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-12-16 22:07:46 +0700

    feat(constants): game speed, piece color, and score

---
+++ src/constant/Contants.ts
@@ -0,0 +1,14 @@
+export const colors = [
+  "#cdd6f4",
+  "#f9e2af",
+  "#89b4fa",
+  "#fab387",
+  "#f38ba8",
+  "#a6e3a1",
+  "#cba6f7",
+  "#89dceb",
+];
+
+export const tetrisScore = [0, 40, 100, 300, 1200];
+
+export const gameSpeed = [0, 1, 10, 50, 100, 250, 500, 1000];
